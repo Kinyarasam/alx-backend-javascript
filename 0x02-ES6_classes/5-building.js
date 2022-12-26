ec9785ccb90692afc7c90d8e82d41f2c24963a81
kinyarasam <skinyara.30@gmail.com>
2022-12-26 14:20:23 +0300

    A Building

--- 0x02-ES6_classes/5-building.js
+++ 0x02-ES6_classes/5-building.js
@@ -1,20 +1,24 @@
 #!/usr/bin/env node
 
+/**
+ * A class representing a Building.
+ */
 export default class Building {
   /**
-   * A class representing a Building.
+   * Constructor for the Building class.
    *
    * @param {number} sqft - The size of the building in square feet.
    */
   constructor(sqft) {
     this.sqft = sqft;
-    if (this.constructor !== 'Building') {
-      if (typeof this.evacuationWarningMessage !== 'function') {
+    /**if (this.constructor !== Building) {
+      if (!typeof this.evacuationWarningMessage !== 'function') {
         throw new Error(
           'Class extending Building must override evacuationWarningMessage',
         );
       }
     }
+    */
   }
 
   /**
@@ -26,9 +30,14 @@ export default class Building {
     return this._sqft;
   }
 
+  /**
+   * Setter for the sqft attribute.
+   *
+   * @param {number} sqft - The new size of the Building in square feet.
+   */
   set sqft(sqft) {
     if (typeof sqft !== 'number') {
-      throw new TypeError('sqft must be an number');
+      throw new TypeError('sqft must be a number');
     }
     this._sqft = sqft;
   }
@@ -37,9 +46,8 @@ export default class Building {
    * implemented to provide an evacuation warning message.
    *
    * @abstract
-   *
+   */
   evacuationWarningMessage() {
     throw new Error('Class extending Building must override evacuationWarningMessage');
   }
-  */
 }
