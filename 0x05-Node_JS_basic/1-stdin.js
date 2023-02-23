d6774049e12fbe4035766f9856cdd382f80caf71
kinyarasam <skinyara.30@gmail.com>
2023-02-23 19:10:37 +0300

    Using Process stdin

---
+++ 0x05-Node_JS_basic/1-stdin.js
@@ -0,0 +1,22 @@
+#!/usr/bin/env node
+/**
+ * Prompts the user for their name and outputs it.
+ * @param {string} message - The message to display to the user
+ * @param {string} name - The name input by the user
+ * @returns {void}
+ */
+process.stdin.setEncoding('utf8');
+
+process.stdout.write('Welcome to Holberton School, what is your name?\n');
+
+process.stdin.on('readable', () => {
+  const chunk = process.stdin.read();
+  if (chunk !== null) {
+    const name = chunk.trim();
+    process.stdout.write(`Your name is: ${name}\n`);
+  }
+});
+
+process.stdin.on('end', () => {
+  process.stdout.write('This important software is now closing\n');
+});
