b62a26d69479b06bce3e0a6bb3b25fd3fef4d7d2
kinyarasam <skinyara.30@gmail.com>
2023-02-24 07:27:14 +0300

    Feat: add HTTP server with plain text response on port 1245
    This commit adds a small http server using the http module in NodeJs.
    The server is assigned to the variable 'app' and exported for use in other modules.
    The server listens on port 12454 and responds with the message 'Hello Holberton School!' as plain text for any endpoint.The code is al
    The code is also documented using JSDoc comments.

---
+++ 0x05-Node_JS_basic/4-http.js
@@ -0,0 +1,37 @@
+#!/usr/bin/env node
+/**
+ * Creates a small http server using the http module
+ * @module 4-http
+ * @exports app
+ */
+const http = require('http');
+
+/**
+ * Port number.
+ * @const {number}
+ */
+const PORT = 1245;
+
+/**
+ * HTTP server
+ * @const
+ */
+const app = http.createServer((req, res) => {
+  res.writeHead(200, { 'Content-Type': 'text/plain' });
+  res.end('Hello Holberton School!\n');
+});
+
+/**
+ * Listen on port
+ * @function
+ * @name listen
+ * @memberof module:4-http~app
+ * @param {number} port - Port number.
+ * @param {Function} callback - Callback function.
+ */
+app.listen(PORT, () => {
+  console.log(`Listening on port ${PORT}`);
+});
+
+
+module.exports = app;
