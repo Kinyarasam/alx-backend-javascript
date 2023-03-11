2ce1225bde3efa299fab0ef98e7b2c3a27eebbe9
kinyarasam <skinyara.30@gmail.com>
2023-03-11 19:47:38 +0300

    Create a small HTTP server using Node's HTTP module

--- 0x05-Node_JS_basic/4-http.js
+++ 0x05-Node_JS_basic/4-http.js
@@ -1,36 +1,11 @@
-#!/usr/bin/env node
-/**
- * Creates a small http server using the http module
- * @module 4-http
- * @exports app
- */
 const http = require('http');
 
-/**
- * Port number.
- * @const {number}
- */
-const PORT = 1245;
-
-/**
- * HTTP server
- * @const
- */
 const app = http.createServer((req, res) => {
-  res.writeHead(200, { 'Content-Type': 'text/plain' });
-  res.end('Hello Holberton School!\n');
+  res.statusCode = 200;
+  res.setHeader('Content-Type', 'text/plain');
+  res.end('Hello Holberton School!');
 });
 
-/**
- * Listen on port
- * @function
- * @name listen
- * @memberof module:4-http~app
- * @param {number} port - Port number.
- * @param {Function} callback - Callback function.
- */
-app.listen(PORT, () => {
-  console.log(`Listening on port ${PORT}`);
-});
+app.listen(1245);
 
 module.exports = app;
