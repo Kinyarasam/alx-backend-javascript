1a3bc5f8647b7cc3eca2661e0f456a2993981c8c
kinyarasam <skinyara.30@gmail.com>
2023-03-11 19:45:36 +0300

    Create a more complex HTTP server using Express

--- 0x05-Node_JS_basic/7-http_express.js
+++ 0x05-Node_JS_basic/7-http_express.js
@@ -1,75 +1,16 @@
-#!/usr/bin/env node
-/**
- * Creates a small express server using the express module
- * @module 7-http_express
- * @exports app
- */
 const express = require('express');
 const countStudents = require('./3-read_file_async');
 
-/**
- * Port number
- * @const {number}
- */
-const PORT = 1245;
-
-/**
- * Database file
- * @const
- */
-const DATABASE = process.argv[2]
-/**
- * Express server
- * @const
- */
 const app = express();
-
-/**
- * Route that returns 'Hello Holberton School!'
- * @name get/
- * @function
- * @memberof module:7-http_express~app
- * @inner
- * @param {object} req - Express request Object.
- * @param {object} res - Express response Object.
- */
-app.get('/', (req, res) => {
-  res.send('Hello Holberton School!');
-});
-
-/**
- * Route that returns 'This is the list of our students'\
- * followed with the same content as the file '3-read_file_async.js'
- * @name get/
- * @function
- * @memberof module:7-http_express~app
- * @inner
- * @param {Object} req - Express request module
- * @param {Object} res - Express response module.
- */
+app.get('/', (req, res) => res.send('Hello Holberton School!'));
 app.get('/students', async (req, res) => {
-
-  console.log(`[200] /students ...`);
-  // res.send('Hello');
-  countStudents(DATABASE)
-  .then((data) => {
-    res.send(data);
-  })
-  .catch((err) => {
-    res.send(err);
-  })
+  const title = 'This is the list of our students\n';
+  try {
+    const data = await countStudents(process.argv[2]);
+    res.send(`${title}${data.join('\n')}`);
+  } catch (error) {
+    res.send(`${title}${error.message}`);
+  }
 });
-
-/**
- * Listen on Port
- * @function
- * @name listen
- * @memberof module:7-http_express~app
- * @param {number} port - Port number.
- * @param {function} callback - Callback function
- */
-app.listen(PORT, () => {
-  console.log(`App is running on port ${PORT}`);
-});
-
+app.listen(1245);
 module.exports = app;
